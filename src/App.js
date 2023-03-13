798b810c85adb2e5df6474e9c09e26de934adf79
cruzetapiko <simonetvia@outlook.com>
2023-03-12 18:11:40 -0600

    calculator finished

--- src/App.js
+++ src/App.js
@@ -3,25 +3,82 @@ import DigitButton from './DigitButton';
 import OperationButton from './OperationButton';
 import {useReducer} from 'react';
 
-
-
 export const ACTIONS = {
   ADD_DIGIT: 'add-digit',
   CLEAR: 'clear',
   DELETE_DIGIT: 'delete-digit',
   CHOOSE_OPERATION: 'choose-operation',
-  EQUALS: 'equals'
+  EQUALS: 'equals' 
 }
 
 function reducer(state, {type, payload}) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
     
-      if (payload.digit === '0' && state.currentOperand === '0') return state;
-      if (payload.digit === '.' && state.currentOperand.includes('.')) return state;
-      if (state.previousOperand === '' && state.currentOperand === '' && payload.digit === '.') return state;
+      if (payload.digit === '0' && state.currentOperand === '0') 
+        return state;
       
-      return {...state,currentOperand: `${state.currentOperand || ""}${payload.digit}`}
+      if (payload.digit === '.' && state.currentOperand.includes('.')) 
+        return state;
+      
+      if (state.previousOperand === '' && state.currentOperand === '' && payload.digit === '.') 
+        return state;
+      
+      return {
+        ...state,currentOperand: `${state.currentOperand || ""}${payload.digit}`
+      }
+
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      } 
+
+      if (state.currentOperand == null) {
+        return {
+           ...state,
+          operation: payload.operation,
+          };    
+      }
+  
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation, 
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+          };
+      }
+    
+      const computation = evaluate(state);
+      return {
+          ...state,
+          operation: payload.operation,
+          currentOperand: null,
+          previousOperand: computation.toString(),
+          };
+
+    case ACTIONS.EQUALS:
+      if (state.currentOperand == null || 
+          state.previousOperand == null ||
+          state.operation == null
+        ) {
+        return state;
+        }
+
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state),
+      } 
+
+    case ACTIONS.CLEAR:
+      return {
+        ...state, currentOperand: null, 
+        previousOperand: null, 
+        operation: null
+      }
 
     case ACTIONS.DELETE_DIGIT:
       if (state.overwrite) {
@@ -40,56 +97,7 @@ function reducer(state, {type, payload}) {
         currentOperand: state.currentOperand.slice(0, -1)
       }
     
-    case ACTIONS.CHOOSE_OPERATION:
-      if (state.currentOperand == null && state.previousOperand == null) {
-        return state;
-      } 
-
-      if (state.currentOperand == null) {
-        return {
-          ...state,
-          operation: payload.operation,
-        };
-        
-      }
-
-      if (state.previousOperand == null) {
-        return {
-          ...state,
-          operation: payload.operation, 
-          previousOperand: state.currentOperand,
-          currentOperand: null,
-         };
-       }
-  
-      const computation = evaluate(state);
-      return {
-         ...state,
-         operation: payload.operation,
-         currentOperand: null,
-         previousOperand: computation.toString(),
-       };
-
-    case ACTIONS.CLEAR:
-      return {...state, currentOperand: null, previousOperand: null, operation: null}
-
-
-    case ACTIONS.EQUALS:
-      if (state.currentOperand == null || 
-          state.previousOperand == null ||
-          state.operation == null
-        ) {
-        return state;
-        }
-
-      return {
-        ...state,
-        overwrite: true,
-        previousOperand: null,
-        operation: null,
-        currentOperand: evaluate(state),
-      } 
-    }
+  }
 }
 
 function evaluate({ currentOperand, previousOperand, operation }) {
@@ -156,3 +164,6 @@ function App() {
 }
 
 export default App;
+
+
+
